b9b93b46b0b789fa783a8aae007546fb9ce5ceb2
Isaac Avila <iavila@inspiracode.net>
2026-08-19 12:11:10 -0600

    Remove .clang-format, update VSCode settings, add areaCriculo.cpp, and modify compile_flags.txt

---
+++ 01-algoritmosFaciles/areaCriculo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float radio, area;
+
+    cout << "Radio del circulo a calcular: ";
+    cin >> radio;
+
+    area = (3.1416) * ((radio * radio));
+    cout << "area: " << area;
+
+}
